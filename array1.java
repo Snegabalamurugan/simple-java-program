991ce46b931bb72a8b3108454acb8c2f4db76866
Snegabalamurugan <snegacse2000@gmail.com>
2022-07-07 12:27:49 +0530

    first commit

---
+++ array1.java
@@ -0,0 +1,12 @@
+public class array1{
+public static void main(string args[])
+{
+int a[]=new int[5];
+a[0]=1;
+a[1]=1;
+a[2]=1;
+a[3]=1;
+for(int i=0;i<a.length;i++)
+System.out.println(a[i]);
+}
+}
